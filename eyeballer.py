a7076d86bc2843f03b4285f4adda9a56cc277ebc
Dan Petro <dpetro@bishopfox.com>
2019-03-06 16:50:51 -0700

    Initial commit  - Some code

---
+++ eyeballer.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python3
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.optimizers import RMSprop
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.callbacks import CSVLogger
+
+MODEL_SUMMARY_FILE = "model_summary.txt"
+TEST_FILE = "test_file.txt"
+MODEL_FILE = "model-vgg16.h5"
+
+# Hyperparams
+IMAGE_SIZE = 200
+IMAGE_WIDTH, IMAGE_HEIGHT = IMAGE_SIZE, IMAGE_SIZE
+EPOCHS = 20
+BATCH_SIZE = 128
+TEST_SIZE = 30
+
+input_shape = (IMAGE_WIDTH, IMAGE_HEIGHT, 3)
+
+# Model 5
+model = Sequential()
+
+"""
+model.add(Conv2D(32, 3, 3, border_mode='same', input_shape=input_shape, activation='relu'))
+model.add(Conv2D(32, 3, 3, border_mode='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+"""
+
+model.add(Conv2D(64, 3, 3, border_mode='same', input_shape=input_shape, activation='relu'))
+model.add(Conv2D(64, 3, 3, border_mode='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+"""
+model.add(Conv2D(64, 3, 3, border_mode='same', activation='relu'))
+model.add(Conv2D(64, 3, 3, border_mode='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+"""
+
+model.add(Conv2D(128, 3, 3, border_mode='same', activation='relu'))
+model.add(Conv2D(128, 3, 3, border_mode='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+"""
+model.add(Conv2D(128, 3, 3, border_mode='same', activation='relu'))
+model.add(Conv2D(128, 3, 3, border_mode='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+"""
+
+model.add(Conv2D(256, 3, 3, border_mode='same', activation='relu'))
+model.add(Conv2D(256, 3, 3, border_mode='same', activation='relu'))
+model.add(Conv2D(256, 3, 3, border_mode='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+"""
+model.add(Conv2D(256, 3, 3, border_mode='same', activation='relu'))
+model.add(Conv2D(256, 3, 3, border_mode='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+"""
+model.add(Conv2D(512, 3, 3, border_mode='same', activation='relu'))
+model.add(Conv2D(512, 3, 3, border_mode='same', activation='relu'))
+model.add(Conv2D(512, 3, 3, border_mode='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(512, 3, 3, border_mode='same', activation='relu'))
+model.add(Conv2D(512, 3, 3, border_mode='same', activation='relu'))
+model.add(Conv2D(512, 3, 3, border_mode='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+            optimizer=RMSprop(lr=0.0001),
+            metrics=['accuracy'])
+
+with open(MODEL_SUMMARY_FILE,"w") as fh:
+    model.summary(print_fn=lambda line: fh.write(line + "\n"))
+print(model.summary())
+
+# Data augmentation
+data_generator = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.1,
+    zoom_range=0.1,
+    validation_split=0.2,
+    horizontal_flip=True)
+
+# Data preparation
+training_generator = data_generator.flow_from_directory(
+    "images/",
+    target_size=(IMAGE_WIDTH, IMAGE_HEIGHT),
+    batch_size=BATCH_SIZE,
+    subset='training',
+    class_mode="binary")
+validation_generator = data_generator.flow_from_directory(
+    "images/",
+    target_size=(IMAGE_WIDTH, IMAGE_HEIGHT),
+    batch_size=BATCH_SIZE,
+    subset='validation',
+    class_mode="binary")
+
+# Training
+model.fit_generator(
+    training_generator,
+    steps_per_epoch=len(training_generator.filenames) // BATCH_SIZE,
+    epochs=EPOCHS,
+    validation_data=validation_generator,
+    validation_steps=len(validation_generator.filenames) // BATCH_SIZE,
+    verbose=1)
+
+model.save_weights(MODEL_FILE)
+
+# Testing
+probabilities = model.predict_generator(validation_generator, TEST_SIZE)
+for index, probability in enumerate(probabilities):
+    image_path = "images/" +validation_generator.filenames[index]
+    img = mpimg.imread(image_path)
+    with open(TEST_FILE,"a") as fh:
+        fh.write(str(probability[0]) + " for: " + image_path + "\n")
+    plt.imshow(img)
+    if probability > 0.5:
+        plt.title("%.2f" % (probability[0]*100) + "% dog")
+    else:
+        plt.title("%.2f" % ((1-probability[0])*100) + "% cat")
+    plt.show()
+
+model.save('model.h5')
+
+model.save_weights("errors_and_logins-weights.h5")
