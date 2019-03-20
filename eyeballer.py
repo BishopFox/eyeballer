a86accbb3301a1646cf6b6794ac8aab988b561a6
Dan Petro <dpetro@bishopfox.com>
2019-03-20 09:35:38 -0700

    Dataframe generator!  - With three classes  - Works pretty well, too

--- eyeballer.py
+++ eyeballer.py
@@ -5,7 +5,7 @@ import matplotlib.image as mpimg
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential, Model
 from keras.optimizers import RMSprop, Adam
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.callbacks import TensorBoard
 from keras import regularizers
@@ -14,8 +14,10 @@ from PIL import Image
 from sklearn.metrics import classification_report, confusion_matrix
 from keras.applications.inception_v3 import InceptionV3
 from keras.applications import MobileNet
+from keras.applications.mobilenet import preprocess_input
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 import os
@@ -25,6 +27,8 @@ import shutil
 # Hyperparams
 #IMAGE_WIDTH, IMAGE_HEIGHT = 360, 225
 IMAGE_WIDTH, IMAGE_HEIGHT = 224, 224
+
+IMAGE_DIR = "images_224x224/"
 
 TEST_FILE = "test_file.txt"
 WEIGHTS_FILE = "weights.h5"
@@ -46,32 +50,33 @@ input_shape = (IMAGE_WIDTH, IMAGE_HEIGHT, 3)
 #base_model = InceptionV3(weights='imagenet', include_top=False, input_shape=input_shape)
 base_model = MobileNet(weights='imagenet', include_top=False, input_shape=input_shape)
 x = base_model.output
-x = Conv2D(3, (2, 2), kernel_regularizer=regularizers.l2(0.01))(x)
-x = MaxPooling2D(pool_size=(2, 2), name="LastPooling")(x)
-x = Dropout(0.2)(x)
+# x = Conv2D(3, (2, 2), kernel_regularizer=regularizers.l2(0.01))(x)
+# x = MaxPooling2D(pool_size=(2, 2), name="LastPooling")(x)
+# x = Dropout(0.2)(x)
 
-x = Flatten()(x)
-
-x = Dense(4, activation="relu", name="HiddenLayer1", kernel_regularizer=regularizers.l2(0.01))(x)
+x = GlobalAveragePooling2D()(x)
+x = Dense(512, activation="relu", name="HiddenLayer1")(x)
+x = Dropout(0.5)(x)
+x = Dense(128, activation="relu", name="HiddenLayer2", kernel_regularizer=regularizers.l2(0.01))(x)
 x = Dropout(0.2)(x)
-x = Dense(4, activation="relu", name="HiddenLayer2", kernel_regularizer=regularizers.l2(0.01))(x)
-x = Dropout(0.2)(x)
-x = Dense(4, activation="relu", name="HiddenLayer3", kernel_regularizer=regularizers.l2(0.01))(x)
-x = Dropout(0.2)(x)
+# x = Dense(4, activation="relu", name="HiddenLayer3", kernel_regularizer=regularizers.l2(0.01))(x)
+# x = Dropout(0.2)(x)
 # x = Dense(8,x = Dense(4, activation="relu", name="HiddenLayer1", kernel_regularizer=regularizers.l2(0.01))(x)
 # x = Dense(16, activation="relu", name="HiddenLayer3", kernel_regularizer=regularizers.l2(0.01))(x)
 # x = Dropout(0.5)(x)
-output_layer = Dense(1, activation="sigmoid", name="OutputLayer")(x)
+output_layer = Dense(3, activation="sigmoid", name="OutputLayer")(x)
 
 model = Model(inputs=base_model.input, outputs=output_layer)
 
-for layer in base_model.layers:
-    layer.trainable = False
+# for layer in base_model.layers:
+#    layer.trainable = False
 
-adam = Adam(lr=.01)
-model.compile(loss='binary_crossentropy',
-              optimizer="rmsprop",
-              metrics=['accuracy'])
+adam = Adam(lr=0.0005)
+model.compile(optimizer=adam,
+    loss="binary_crossentropy",
+    metrics=["accuracy"])
+
+print(model.summary())
 
 if args.modelfile:
     if os.path.isfile(WEIGHTS_FILE):
@@ -80,32 +85,43 @@ if args.modelfile:
     else:
         print("No model to load from file")
 
-print(model.summary())
-
 # Data augmentation
 data_generator = ImageDataGenerator(
+    preprocessing_function = preprocess_input,
     rescale=1./255,
     shear_range=0.2,
     zoom_range=0.2,
-#    color_mode="grayscale",
     samplewise_center=True,
     validation_split=0.2,
     horizontal_flip=False)
 
+# data_generator = ImageDataGenerator(preprocessing_function = preprocess_input,
+#     validation_split=0.2)
+
 # Data preparation
-training_generator = data_generator.flow_from_directory(
-    "images/",
+
+data = pd.read_csv("labels.csv")
+
+training_generator = data_generator.flow_from_dataframe(
+    data,
+    directory=IMAGE_DIR,
+    x_col="filename",
+    y_col=["custom404", "login", "homepage"],
     target_size=(IMAGE_WIDTH, IMAGE_HEIGHT),
     batch_size=BATCH_SIZE,
     subset='training',
-    class_mode="binary")
-validation_generator = data_generator.flow_from_directory(
-    "images/",
+    shuffle=True,
+    class_mode="other")
+validation_generator = data_generator.flow_from_dataframe(
+    data,
+    directory=IMAGE_DIR,
+    x_col="filename",
+    y_col=["custom404", "login", "homepage"],
     target_size=(IMAGE_WIDTH, IMAGE_HEIGHT),
     batch_size=BATCH_SIZE,
     subset='validation',
     shuffle=False,
-    class_mode="binary")
+    class_mode="other")
 
 # Training
 history = model.fit_generator(
@@ -115,6 +131,10 @@ history = model.fit_generator(
     validation_data=validation_generator,
     validation_steps=len(validation_generator.filenames) // BATCH_SIZE,
     verbose=1)
+
+results = model.predict_generator(validation_generator)
+print("Results: ", results)
+print("len Results: ", len(results))
 
 # Plot training & validation accuracy values
 plt.plot(history.history['acc'])
@@ -142,26 +162,26 @@ if args.modelfile:
     model.save_weights(WEIGHTS_FILE)
     print("Model saved")
 
-def confusion_matrix_evaluate(validation_generator, model):
-    #Confution Matrix and Classification Report
-    y_pred = model.predict_generator(validation_generator, verbose=1)
-    y_pred = y_pred > 0.5
-    print('Confusion Matrix')
-    print(confusion_matrix(validation_generator.classes, y_pred))
-
-    j = 0
-    for batch in validation_generator:
-        labels = batch[1]
-        labels = labels > 0.5
-        for i in range(len(labels)):
-            index = i + (j * BATCH_SIZE)
-            # Correct?
-            if labels[i] == y_pred[index]:
-                shutil.copyfile("images/" + validation_generator.filenames[index], "confusion/correct/" + validation_generator.filenames[index])
-            # Wrong
-            else:
-                shutil.copyfile("images/" + validation_generator.filenames[index], "confusion/wrong/" + validation_generator.filenames[index])
-        j+=1
-
-# Print out a confusion matrix in image form!
-confusion_matrix_evaluate(validation_generator, model)
+# def confusion_matrix_evaluate(validation_generator, model):
+#     #Confution Matrix and Classification Report
+#     y_pred = model.predict_generator(validation_generator, verbose=1)
+#     y_pred = y_pred > 0.5
+#     print('Confusion Matrix')
+#     print(confusion_matrix(validation_generator.classes, y_pred))
+#
+#     j = 0
+#     for batch in validation_generator:
+#         labels = batch[1]
+#         labels = labels > 0.5
+#         for i in range(len(labels)):
+#             index = i + (j * BATCH_SIZE)
+#             # Correct?
+#             if labels[i] == y_pred[index]:
+#                 shutil.copyfile(IMAGE_DIR + validation_generator.filenames[index], "confusion/correct/" + validation_generator.filenames[index])
+#             # Wrong
+#             else:
+#                 shutil.copyfile(IMAGE_DIR + validation_generator.filenames[index], "confusion/wrong/" + validation_generator.filenames[index])
+#         j+=1
+#
+# # Print out a confusion matrix in image form!
+# confusion_matrix_evaluate(validation_generator, model)
