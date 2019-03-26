753347a6e91359fc06ed048330be574659d7dc44
Dan Petro <dpetro@bishopfox.com>
2019-03-25 17:15:11 -0700

    Object oriented now  - Much cleaner  - Positional mode argument

--- eyeballer.py
+++ eyeballer.py
@@ -26,196 +26,150 @@ import argparse
 import shutil
 import time
 
-# Hyperparams
-#IMAGE_WIDTH, IMAGE_HEIGHT = 360, 225
-IMAGE_WIDTH, IMAGE_HEIGHT = 224, 224
-
-IMAGE_DIR = "images_224x224/"
-
-TEST_FILE = "test_file.txt"
-WEIGHTS_FILE = "weights.h5"
-CHECKPOINT_FILE="weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
-
 # Parse the arguments
 parser = argparse.ArgumentParser(description='Give those screenshots of yours a quick eyeballing')
-parser.add_argument("--modelfile", help="Weights file for input/output")
+parser.add_argument("mode", help="Mode of operation. One of \"train\" or \"predict\".")
+parser.add_argument("--weights", help="Weights file for input/output")
 parser.add_argument("--batchsize", help="Batch size", default=32, type=int)
 parser.add_argument("--epochs", help="Number of epochs", default=20, type=int)
-parser.add_argument("--predict", help="File to predict")
+parser.add_argument("--screenshot", help="File to predict")
+parser.add_argument("--summary", help="Print model summary at start")
+parser.add_argument("--graphs", help="Save accuracy and loss graphs to file")
+
 args = parser.parse_args()
 
-if args.modelfile:
-    WEIGHTS_FILE = args.modelfile
-BATCH_SIZE = args.batchsize
-EPOCHS = args.epochs
+class EyeballModel:
+    checkpoint_file = "weights-snapshot.h5"
+    image_dir = "images_224x224/"
+    image_width, image_height = 224, 224
+    input_shape = (image_width, image_height, 3)
 
-input_shape = (IMAGE_WIDTH, IMAGE_HEIGHT, 3)
+    def __init__(self, print_summary=False, weights_file="weights.h5", summary=False, graphs=False):
+        # Build the model
+        base_model = MobileNet(weights='imagenet', include_top=False, input_shape=self.input_shape)
+        x = base_model.output
+        x = GlobalAveragePooling2D()(x)
+        x = Dense(512, activation="relu", name="HiddenLayer1")(x)
+        x = Dropout(0.5)(x)
+        x = Dense(128, activation="relu", name="HiddenLayer2", kernel_regularizer=regularizers.l2(0.01))(x)
+        x = Dropout(0.2)(x)
+        output_layer = Dense(3, activation="sigmoid", name="OutputLayer")(x)
+        self.model = Model(inputs=base_model.input, outputs=output_layer)
 
-#base_model = InceptionV3(weights='imagenet', include_top=False, input_shape=input_shape)
-base_model = MobileNet(weights='imagenet', include_top=False, input_shape=input_shape)
-x = base_model.output
-# x = Conv2D(3, (2, 2), kernel_regularizer=regularizers.l2(0.01))(x)
-# x = MaxPooling2D(pool_size=(2, 2), name="LastPooling")(x)
-# x = Dropout(0.2)(x)
+        adam = Adam(lr=0.0005)
+        self.model.compile(optimizer=adam, loss="binary_crossentropy", metrics=["accuracy"])
 
-x = GlobalAveragePooling2D()(x)
-x = Dense(512, activation="relu", name="HiddenLayer1")(x)
-x = Dropout(0.5)(x)
-x = Dense(128, activation="relu", name="HiddenLayer2", kernel_regularizer=regularizers.l2(0.01))(x)
-x = Dropout(0.2)(x)
-# x = Dense(4, activation="relu", name="HiddenLayer3", kernel_regularizer=regularizers.l2(0.01))(x)
-# x = Dropout(0.2)(x)
-# x = Dense(8,x = Dense(4, activation="relu", name="HiddenLayer1", kernel_regularizer=regularizers.l2(0.01))(x)
-# x = Dense(16, activation="relu", name="HiddenLayer3", kernel_regularizer=regularizers.l2(0.01))(x)
-# x = Dropout(0.5)(x)
-output_layer = Dense(3, activation="sigmoid", name="OutputLayer")(x)
+        if summary:
+            print(self.model.summary())
 
-model = Model(inputs=base_model.input, outputs=output_layer)
+        if weights_file is not None and os.path.isfile(weights_file):
+            self.model.load_weights(weights_file)
+            print("Loaded model from file.")
+        else:
+            print("No model to load from file")
 
-# Single predict mode
-if args.predict:
-    # Load the model from file
-    model.load_weights(WEIGHTS_FILE)
+    def train(self, weights_file, epochs=20, batch_size=32):
+        # Data augmentation
+        data_generator = ImageDataGenerator(
+            preprocessing_function = preprocess_input,
+            rescale=1./255,
+            shear_range=0.0,
+            zoom_range=0.2,
+            samplewise_center=True,
+            validation_split=0.2,
+            horizontal_flip=False)
 
-    # Load the image into memory
-    img = image.load_img(args.predict, target_size=(IMAGE_WIDTH, IMAGE_HEIGHT))
-    img = image.img_to_array(img)
-    img = np.expand_dims(img, axis=0)
-    img = preprocess_input(img)
-    
-    time_start = time.time()
-    prediction = model.predict(img, batch_size=1)
-    time_end = time.time()
-    elapsed = time_end - time_start
-    print("Predictions:")
-    print("\tCustom 404:", round(prediction[0][0] * 100, 2))
-    print("\tLogin Page:", round(prediction[0][1] * 100, 2))
-    print("\tHome Page:", round(prediction[0][2] * 100, 2))
-    print("Prediction Took (seconds): ", elapsed)
-    exit()
+        data = pd.read_csv("labels.csv")
 
-# for layer in base_model.layers:
-#    layer.trainable = False
+        training_generator = data_generator.flow_from_dataframe(
+            data,
+            directory=self.image_dir,
+            x_col="filename",
+            y_col=["custom404", "login", "homepage"],
+            target_size=(self.image_width, self.image_height),
+            batch_size=batch_size,
+            subset='training',
+            shuffle=True,
+            class_mode="other")
+        validation_generator = data_generator.flow_from_dataframe(
+            data,
+            directory=self.image_dir,
+            x_col="filename",
+            y_col=["custom404", "login", "homepage"],
+            target_size=(self.image_width, self.image_height),
+            batch_size=batch_size,
+            subset='validation',
+            shuffle=False,
+            class_mode="other")
 
-adam = Adam(lr=0.0005)
-model.compile(optimizer=adam,
-    loss="binary_crossentropy",
-    metrics=["accuracy"])
+        # Training
 
-print(model.summary())
+        # Model checkpoint - Saves model weights when validation accuracy improves
+        callbacks = []
+        callbacks.append(ModelCheckpoint(self.checkpoint_file, monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=True, mode='min'))
 
-if args.modelfile:
-    if os.path.isfile(WEIGHTS_FILE):
-        model.load_weights(WEIGHTS_FILE)
-        print("Loaded model from file.")
+        history = self.model.fit_generator(
+            training_generator,
+            steps_per_epoch=len(training_generator.filenames) // batch_size,
+            epochs=epochs,
+            validation_data=validation_generator,
+            validation_steps=len(validation_generator.filenames) // batch_size,
+            callbacks=callbacks,
+            verbose=1)
+
+        if graphs:
+            # Plot training & validation accuracy values
+            plt.plot(history.history['acc'])
+            plt.plot(history.history['val_acc'])
+            plt.title('Model accuracy')
+            plt.ylabel('Accuracy')
+            plt.xlabel('Epoch')
+            plt.legend(['Train', 'Validation'], loc='upper left')
+            plt.savefig("accuracy.png")
+            plt.clf()
+            plt.cla()
+            plt.close()
+
+            # Plot training & validation loss values
+            plt.plot(history.history['loss'])
+            plt.plot(history.history['val_loss'])
+            plt.title('Model loss')
+            plt.ylabel('Loss')
+            plt.xlabel('Epoch')
+            plt.legend(['Train', 'Validation'], loc='upper left')
+            plt.savefig("loss.png")
+
+
+    # Single predict on a file
+    def predict(self, filename):
+        # Load the weights from file
+        self.model.load_weights(self.weights_file)
+
+        # Load the image into memory
+        img = image.load_img(filename, target_size=(self.image_width, self.image_height))
+        img = image.img_to_array(img)
+        img = np.expand_dims(img, axis=0)
+        img = preprocess_input(img)
+
+        time_start = time.time()
+        prediction = self.model.predict(img, batch_size=1)
+        time_end = time.time()
+        print("Predictions:")
+        print("\tCustom 404:", round(prediction[0][0] * 100, 2))
+        print("\tLogin Page:", round(prediction[0][1] * 100, 2))
+        print("\tHome Page:", round(prediction[0][2] * 100, 2))
+        print("Prediction Took (seconds): ", time_end - time_start)
+
+
+graphs = args.graphs is not None
+
+model = EyeballModel(weights_file=args.weights, graphs=graphs)
+if args.mode == "train":
+    model.train(weights_file=args.weights, batch_size=args.batchsize, epochs=args.epochs)
+elif args.mode == "predict":
+    if args.screenshot is None:
+        print("Error: Use the --screenshot flag to specify what file you want to predict on")
     else:
-        print("No model to load from file")
-
-# Data augmentation
-data_generator = ImageDataGenerator(
-    preprocessing_function = preprocess_input,
-    rescale=1./255,
-    shear_range=0.0,
-    zoom_range=0.2,
-    samplewise_center=True,
-    validation_split=0.2,
-    horizontal_flip=False)
-
-# data_generator = ImageDataGenerator(preprocessing_function = preprocess_input,
-#     validation_split=0.2)
-
-# Data preparation
-
-data = pd.read_csv("labels.csv")
-
-training_generator = data_generator.flow_from_dataframe(
-    data,
-    directory=IMAGE_DIR,
-    x_col="filename",
-    y_col=["custom404", "login", "homepage"],
-    target_size=(IMAGE_WIDTH, IMAGE_HEIGHT),
-    batch_size=BATCH_SIZE,
-    subset='training',
-    shuffle=True,
-    class_mode="other")
-validation_generator = data_generator.flow_from_dataframe(
-    data,
-    directory=IMAGE_DIR,
-    x_col="filename",
-    y_col=["custom404", "login", "homepage"],
-    target_size=(IMAGE_WIDTH, IMAGE_HEIGHT),
-    batch_size=BATCH_SIZE,
-    subset='validation',
-    shuffle=False,
-    class_mode="other")
-
-# Training
-
-#    Model checkpoint code - Saves model weights when validation accuracy improves
-checkpoint = ModelCheckpoint(CHECKPOINT_FILE, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-callbacks_list = [checkpoint]
-
-start_time = time.time()
-
-history = model.fit_generator(
-    training_generator,
-    steps_per_epoch=len(training_generator.filenames) // BATCH_SIZE,
-    epochs=EPOCHS,
-    validation_data=validation_generator,
-    validation_steps=len(validation_generator.filenames) // BATCH_SIZE,
-    callbacks=callbacks_list,
-    verbose=1)
-
-end_time = time.time()
-print("Total training time: {}".format(end_time-start_time))
-
-if args.modelfile:
-    print("Saving model...")
-    model.save_weights(WEIGHTS_FILE)
-    print("Model saved")
-
-# Plot training & validation accuracy values
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Validation'], loc='upper left')
-plt.savefig("accuracy.png")
-plt.clf()
-plt.cla()
-plt.close()
-
-# Plot training & validation loss values
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model loss')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Validation'], loc='upper left')
-plt.savefig("loss.png")
-
-
-# def confusion_matrix_evaluate(validation_generator, model):
-#     #Confution Matrix and Classification Report
-#     y_pred = model.predict_generator(validation_generator, verbose=1)
-#     y_pred = y_pred > 0.5
-#     print('Confusion Matrix')
-#     print(confusion_matrix(validation_generator.classes, y_pred))
-#
-#     j = 0
-#     for batch in validation_generator:
-#         labels = batch[1]
-#         labels = labels > 0.5
-#         for i in range(len(labels)):
-#             index = i + (j * BATCH_SIZE)
-#             # Correct?
-#             if labels[i] == y_pred[index]:
-#                 shutil.copyfile(IMAGE_DIR + validation_generator.filenames[index], "confusion/correct/" + validation_generator.filenames[index])
-#             # Wrong
-#             else:
-#                 shutil.copyfile(IMAGE_DIR + validation_generator.filenames[index], "confusion/wrong/" + validation_generator.filenames[index])
-#         j+=1
-#
-# # Print out a confusion matrix in image form!
-# confusion_matrix_evaluate(validation_generator, model)
+        model.predict(args.screenshot)
+else:
+    print("Error: " + args.mode + " is not a valid mode. Try one of \"train\" or \"predict\"")
