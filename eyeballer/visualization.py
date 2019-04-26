d3c4e7fc494db3459bf376ea1d19c33ccccc614f
Dan Petro <dpetro@bishopfox.com>
2019-04-26 09:59:25 -0700

    Saving progress. Reset this

---
+++ eyeballer/visualization.py
@@ -0,0 +1,16 @@
+from model import EyeballModel
+
+
+def heatmap(model, screenshot):
+    # Loop over a grid overlaid on top of the image
+
+    partial_image = _occlude(screenshot, x, y)
+    pass
+
+
+def _occlude(screenshot, x, y):
+    """ Occlude an image
+
+    """
+
+    pass
