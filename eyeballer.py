1213c5d3f118bd60330ffb7513a89644ab6a25cf
Gargz <bivanenko@bishopfox.com>
2019-06-19 17:31:19 -0400

    Got rid of html_support and added paging

--- eyeballer.py
+++ eyeballer.py
@@ -57,72 +57,238 @@ def predict(ctx, screenshot, heatmap, threshold):
             labelwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
             labelwriter.writeheader()
             labelwriter.writerows(results)
-        
 
         print("Output written to results.csv")
 
-        buildHTML(processResults(results, threshold))
-        print("HTML is created in results.html")
+        buildHTML(processResults(results));
+        print("HTML written to results.html")
 
 
-def processResults(results, threshold):
-    '''Filter the initial results dictionary according to threshold and reformat it for use in buildHTML.
+def processResults(results):
+    '''Filter the initial results dictionary and reformat it for use in JS.
 
         Keyword arguments:
         results -- dictionary output from predict function
-        threshold -- argument that specifies minimum acceptabe level of confidence (Ex. eyeballer ... --threshold 0.5) 
-
-    '''
-    procesedResults = []
-
-    for result in results:
-        positive_label = ''
-        pic_info = {}
-
-        for screenshot_label, label in result.items():
-
-            if screenshot_label == 'filename':
-                pic_info['filename'] = label
-                pic_info['tags'] = ''
-
-            elif label > threshold:
-                positive_label += (str(screenshot_label) + ' ') 
-
-        pic_info['tags'] += positive_label
-        procesedResults.append(pic_info)
-
-    return procesedResults
-
-
-def buildHTML(results):
-    '''Build HTML by iterating through filtered dictionary and save it under specified name.
-
-        Keyword arguments:
-        results -- dictionary output from processResults function
         
     '''
-    html_main = '''
-    <link rel="stylesheet" type="text/css" href="html_support/style.css">
-    <div id="btnsCont">
-      <button class="btn all" onclick="filterSelection('all')"> Show all</button>
-      <button class="btn" onclick="filterSelection('404')"> 404</button>
-      <button class="btn" onclick="filterSelection('login')"> Login</button>
-      <button class="btn" onclick="filterSelection('home')"> Home Page</button>
-      <button class="btn" onclick="filterSelection('old')"> Old Looking</button>
-    </div>
-    <div class="container">
-    '''
-    
-    for pic_info in results:
-        html_main += ('<div class="filterDiv ' + pic_info['tags'] + 'show"><a href=' + pic_info['filename']  + '>'+ pic_info['tags'].upper() + '</a><img class="res_pic" src="' + pic_info['filename'] + '"></div>\n')
+    jsResults = {}
 
-    html_main += '''
-    </div>
-    <script src="html_support/javasc.js"></script>
+    for result in results:
+        positiveTags=[]
+
+        for label, label_info in result.items():
+            if (label == 'filename'):
+                pass
+            
+
+            elif (label_info == True) :
+            
+                positiveTags.append(label)
+
+        jsResults[result['filename']]= positiveTags
+    return(jsResults)
+
+def buildHTML(jsResults):
+    '''Build HTML around the JS Dictionary that is passed from processResults.
+
+        Keyword arguments:
+        jsResults -- dictionary output from processResults function
     '''
-    
+    html_main =('''
+    <!DOCTYPE HTML>
+    <html>
+    <title>Eyeballer Results</title>
+    <body>
+        <div id="btnsCont">
+          <button class="btn all" onclick="filterSelection('all')"> Show all</button>
+          <button class="btn" onclick="filterSelection('custom404')"> 404</button>
+          <button class="btn" onclick="filterSelection('login')"> Login</button>
+          <button class="btn" onclick="filterSelection('homepage')"> Home Page</button>
+          <button class="btn" onclick="filterSelection('oldlooking')"> Old Looking</button>
+            <select onchange="numOnPage=this.value; filterSelection([]);" style="float:right; margin:5px; margin-right:30px; height:30px; width:auto;">
+              <option value="25">25</option>
+              <option value="50">50</option>
+              <option value="100">100</option>
+              <option value="250">250</option>
+            </select>
+        </div>
+        <div id="btnsPage">
+        </div>
+        <div id="container">
+        </div>
+        <button onclick="topFunction()" id="topBtn" title="Go to top">Top</button>
+    </body> 
+    <script>
+        var results = ''' + str(jsResults) + ''';
+        var selectedTags = new Array();
+        var btnContainer = document.getElementById("btnsCont");
+        var categoryBtns = btnContainer.getElementsByClassName("btn");
+        var numOnPage = 100; 
+        var indexesToDisplay = [];
+        filterSelection([]);
+        for (var i = 0; i < categoryBtns.length; i++) {
+            categoryBtns[i].addEventListener("click", function () {
+                if (this.className.indexOf("active") > -1) {
+                    this.className = this.className.replace(" active", "");
+                }
+                else {
+                this.className += " active";
+                }
+            });
+        }
+        function filterSelection(c) {
+            indexesToDisplay = [];
+            var currentIndex = -1;
+            if (c == "all") location.reload();
+            if (selectedTags.includes(c)!=true) selectedTags=selectedTags.concat(c);
+            else selectedTags.splice(selectedTags.indexOf(c),1);
+            for (var filename in results){
+                    currentIndex++; 
+                    var tags = results[filename];
+                    var isMissingTag = false;
+                    for (var i in selectedTags){
+                        if (!tags.includes(selectedTags[i])){
+                            isMissingTag = true;
+                            break;
+                        }
+                    }
+                    if (isMissingTag === false){
+                        indexesToDisplay = indexesToDisplay.concat(currentIndex);
+                    }
+                }
+            buildPageButtons(indexesToDisplay);
+            buildHTML(0, numOnPage);
+        }
+        function buildPageButtons(indexes){
+            numOfPages = Math.ceil(indexes.length/numOnPage);
+            var html = '';
+            for (var i = 0; i < numOfPages; i++){
+                html += ('<button class="btnpage" onclick="buildHTML(' + (i*numOnPage) + ', ' + ((i+1)*numOnPage) + '); activatePage(this);">' + (i+1) +'</button>');
+            }
+            document.getElementById('btnsPage').innerHTML = html;
+        }
+        function activatePage(pressedBtn){
+            var activeNow = document.getElementsByClassName('activePage')
+            for (var i = 0; i < activeNow.length; i++){
+                activeNow[i].className = activeNow[i].className.replace(" activePage", "");
+            }
+            pressedBtn.className += ' activePage';
+        }
+        function buildHTML(rangeMin, rangeMax){
+            var html = '';
+            if (rangeMax > indexesToDisplay.length) rangeMax=indexesToDisplay.length;
+            for (var i = rangeMin; i < rangeMax; i++){
+                index=indexesToDisplay[i];
+                filename = Object.keys(results)[index];
+                html += ('<div class="filterDiv"><a href=' + filename + ' target="_blank"> ' + results[filename].toString().replace(/,/g, ' ') + '</a><img class="res_pic" src="' + filename + '"></div>');
+            }
+            document.getElementById('container').innerHTML = html;
+        }   
+        window.onscroll = function() {scrollFunction()};
+        function scrollFunction() {
+          if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById("topBtn").style.display = "block";
+          } else {
+            document.getElementById("topBtn").style.display = "none";
+          }
+        }
+        function topFunction() {
+          document.body.scrollTop = 0; // For Safari
+          document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        }
+        </script>
+        <style>
+        #topBtn {
+          display: none; /* Hidden by default */
+          position: fixed; /* Fixed/sticky position */
+          bottom: 20px; /* Place the button at the bottom of the page */
+          right: 30px; /* Place the button 30px from the right */
+          z-index: 99; /* Make sure it does not overlap */
+          border: none; /* Remove borders */
+          outline: none; /* Remove outline */
+          background-color: #193b72; /* Set a background color */
+          color: white; /* Text color */
+          cursor: pointer; /* Add a mouse pointer on hover */
+          padding: 15px; /* Some padding */
+          border-radius: 10px; /* Rounded corners */
+          font-size: 18px; /* Increase font size */
+          font-weight:bold;
+          border:solid;
+        }
+        #topBtn:hover {
+          background-color: #555; 
+        }
+        .container {
+        overflow: hidden;
+        }
+        #btnsPage {
+        margin:10px;
+        }
+        .btnpage{
+            width:40px;
+            height:30px;
+            background-color: #f1f1f1;
+            outline: none;
+        }
+        .res_pic{
+        width:100%;
+        height:auto;
+        }
+        a{
+        font-weight:600;
+        padding:3px;
+        text-decoration: none;
+        color:#ffffff;
+        font-size:16px;
+        font-family:Sans-serif;
+        }
+        .name{
+        float:left;
+        }
+        .filterDiv {
+          float: left;
+          background-color: #777;
+          color: #ffffff;
+          width: 22%;
+          border-style:solid;
+          border-color:#666;
+          text-align: center;
+          margin: 10px;
+          line-height: 30px;
+          display: block;
+        }
+        .btn {
+          border: none;
+          outline: none;
+          padding: 12px 16px;
+          margin-left:10px;
+          background-color: #f1f1f1;
+          cursor: pointer;
+        }
+        .btn:hover {
+          background-color: #ddd;
+        }
+        .active {
+          background-color: #666;
+          color: white;
+        }
+        .activePage{
+            border: solid;
+            border-color:#666666;
+            color: black;
+        }
+        .btn.all {
+          background-color: #193b72;
+          color: white;
+        } 
+        </style>
+    </html>
+    ''');
+
     with open('results.html', 'w') as file:
-        file.write(html_main)
+            file.write(html_main)
+
+
 
 
 def pretty_print_evaluation(results):
