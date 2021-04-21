92738b16a468fa3a1cfd14c4964ff720ba32cc7f
Dan Petro <dpetro@bishopfox.com>
2021-04-21 16:23:30 -0700

    Add verify util script

--- utils/resizer.sh
+++ utils/resizer.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Converts full-size images to a given size (replacing the originals)
-for f in images_full_size/*
+for f in images/*
 do
+	echo $f
 	convert -resize 224x224\! $f $f
 done
